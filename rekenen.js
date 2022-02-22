f883241f49e07d78799e28910ba95f482ce1b261
steffffffffff <6020787@mborijnland.nl>
2022-02-22 09:56:40 +0100

    optellen

---
+++ rekenen.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+function optellen(){
+
+    let getal1 = document.getElementById("optellen1").value;
+    let getal2 = document.getElementById("optellen2").value;
+
+
+    document.getElementById("antwoord").innerHTML = +getal1 + +getal2;
+
+    
+    
+}
+
+document.getElementById('bereken').addEventListener('click', optellen);
