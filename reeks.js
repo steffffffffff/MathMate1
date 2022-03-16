5996da5b90b3e8112b6a5914a7651408229cefe7
steffffffffff <6020787@mborijnland.nl>
2022-03-16 12:16:44 +0100

    reeks

--- reeks.js
+++ reeks.js
@@ -45,9 +45,31 @@ function Fibonacci(){
     let getal = document.getElementById("fibo1").value;
     let n1 = 0;
     let n2 = 1;
-    let n3 = n1 + n2;
-    document.getElementById("antwoord5").innerHTML = " ";
-    while(n2 < getal){
-        document.getElementById("antwoord5").innerHTML += n3 + "<br>",n1++,n2++;
+    let n3;
+    document.getElementById("antwoord5").innerHTML = "0<br>1<br>";
+    for(let i = 1; i<=getal;i++){
+        document.getElementById("antwoord5").innerHTML += (n3 = n1 + n2) + "<br>",n1=n2,n2=n3;
+        
+        
     }
 }document.getElementById('bereken5').addEventListener('click', Fibonacci);
+
+
+function isPrime() {
+    document.getElementById("antwoord6").innerHTML = " ";
+    let getal = document.getElementById("prime1").value
+    for (var counter = 2; counter <= getal; counter++) {
+
+        var notPrime = false;
+        for (var i = 2; i <= counter; i++) {
+            if (counter%i===0 && i!==counter) {
+                notPrime = true;
+            }
+        }
+        if (notPrime === false) {
+                    document.getElementById("antwoord6").innerHTML += counter + "<br>";
+        }
+    }
+    
+    }document.getElementById('bereken6').addEventListener('click', isPrime);
+    
