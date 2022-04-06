a3398b9fb37e2952cbd92e0bc8521243b50f0350
steffffffffff <6020787@mborijnland.nl>
2022-04-06 10:38:49 +0200

    game

--- game.js
+++ game.js
@@ -33,21 +33,25 @@ function attack(){
     let lost = false
     let won = false
     let rng = Math.ceil(Math.random() * 8);
-    let bchance = Math.ceil(Math.random() * 12);
+    let bchance = Math.ceil(Math.random() * 8);
     let boss = false
     let enemyrng = Math.ceil(Math.random() * 10);
     //dealing damage
     
-    if (enemy.hp > 0){
+    
+    if (enemy.hp >= 0){
         if(rng != 8){
         document.getElementById("enemyhp").innerHTML = "hp: " + (enemy.hp = (enemy.hp - player.damage));
         document.getElementById("info").innerHTML = "hit!"
     }else{document.getElementById("enemyhp").innerHTML = "hp: " + (enemy.hp = enemy.hp - (player.damage*2))
 document.getElementById("info").innerHTML = "CRIT!"}
     
+
+    //enemy dies
         if (enemy.hp <= 0){
             document.getElementById("info").innerHTML = "win!";
             if(boss == true){
+                document.getElementById("enemy3").style
                 document.getElementById("playerxp").innerHTML = "xp: "+(player.xp = player.xp + 9)+ "/"+ player.maxxp;
                 boss = false;
             }
@@ -60,11 +64,10 @@ document.getElementById("info").innerHTML = "CRIT!"}
             if(player.xp >= player.maxxp){
                 player.xp = 0;
                 document.getElementById("info").innerHTML = "lvl up!";
-                player.damage = player.damage + (Math.floor(player.damage*0.5) + 2);
+                player.damage = player.damage + (Math.floor(player.damage*1.5) + 2);
                 player.maxhp = player.maxhp + (Math.floor(player.maxhp/10)+ 5)
-                player.magic = player.magic + (Math.floor(player.magic*0.5)+ 1)
                 document.getElementById("lvl").innerHTML = "lvl: "+ (player.lvl = player.lvl + 1);
-                document.getElementById("playerxp").innerHTML = "xp: "+player.xp+ "/"+ (player.maxxp = player.maxxp + 5);
+                document.getElementById("playerxp").innerHTML = "xp: "+player.xp+ "/"+ (player.maxxp = player.maxxp + Math.floor(player.maxxp*0.5)+3);
                 
             }
         }
@@ -72,7 +75,6 @@ document.getElementById("info").innerHTML = "CRIT!"}
 
     // player out levels the enemy
     if((player.lvl*2) > enemy.lvl){
-        enemy.damage = enemy.damage + 1;
         enemy.maxhp = enemy.maxhp + 5;
         enemy.lvl = enemy.lvl + 1;
     }
@@ -87,17 +89,25 @@ document.getElementById("info").innerHTML = "CRIT!"}
 
     //reset battle
     if(won == true){
+
+        document.getElementById("enemy1").style.display = "none";
+        document.getElementById("enemy2").style.display = "contents";
         enemy.hp = enemy.maxhp
 
-        if(player.lvl >= 5){
-        if(bchance == 1){
-            enemy.hp = enemy.hp * 2.5;
-            enemy.damage = enemy.damage * 2.5;
+        if(player.lvl >= 3){
+        if(bchance = 1){
+            enemy.hp = enemy.hp * 2;
+            enemy.damage = enemy.damage * 2;
             boss = true;
             
             player.hp = player.maxhp;
+            
         }
     }
+    }else{
+       
+        document.getElementById("enemy1").style.display = "contents";
+        document.getElementById("enemy2").style.display = "none";
     }
 }document.getElementById("attack1").addEventListener('click', attack);
 
